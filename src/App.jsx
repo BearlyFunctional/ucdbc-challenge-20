2a0f3238151652b7bc74f8be8cb687ff8c67f289
BearlyFunctional <hjbell123.hb@gmail.com>
2024-02-20 21:29:51 -0800

    Added very barebones starter code. (Life is hard)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className="header">
+            <Header />
+        </div>
+    );
+}
+
+export default App;
